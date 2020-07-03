feab339e016ff2b07b4f50e4a15396e8e73e6de5
Artem Osintsev <kybernet@bk.ru>
2020-07-04 01:13:13 +0700

    fix links in templates files (url_for jinja)

--- app.py
+++ app.py
@@ -45,8 +45,8 @@
         https://github.com/kushedow/flask-html/blob/master/Project%201/data.py
 
 """
-import json
-from flask import Flask, render_template  # подключаем модули
+
+from flask import Flask, render_template
 from data import tours as tour, departures as depart, subtitle, description, title
 
 app = Flask(__name__)  # объявляем экземпляр фласка
@@ -55,26 +55,32 @@ app = Flask(__name__)  # объявляем экземпляр фласка
 @app.route('/')  # объявим путь к главной странице
 def index():
     return render_template('index.html', title=title, tours=tour, subtitle=subtitle, departures=depart,
-                           description=description)  # используем шалбон страницы
+                           description=description)
 
 
-@app.route('/departures/<departure>')  # путь отобразить направления
+@app.route('/departures/<string:departure>')  # путь отобразить направления
 def departures(departure):
-    lst_depart = []
+
+    lst_depart = []    # список индексов туров из соответстующих направлению
+    price_depart = []  # список цен подходящих туров
+    count_nights = []  # список кол-во ночей подходящих туров
+
     for t in tour:
         if departure == tour[t]['departure']:
             lst_depart.append(t)
+            price_depart.append(tour[t]['price'])
+            count_nights.append(tour[t]['nights'])
     return render_template('departure.html', title=title, tours=tour, subtitle=subtitle,
                            lst_departures=lst_depart, departures=depart, city=departure,
-                           description=description)
+                           description=description, price_depart=price_depart, count_nights=count_nights)
 
 
 @app.route('/tours/<int:id>/')  # путь просмотра тура
 def tours(id):
-    return render_template('tour.html', title=tour[id]['title'],
+    return render_template('tour.html', title=title, tour_title=tour[id]['title'],
                            country=tour[id]['country'], departures=depart[tour[id]['departure']], lst_departures=depart,
                            nights=tour[id]['nights'], description=tour[id]['description'],
-                           picture=tour[id]['picture'], price=tour[id]['price'])
+                           picture=tour[id]['picture'], price=tour[id]['price'], stars=tour[id]['stars'])
 
 
 app.run('0.0.0.0', 8000, debug=True)  # запустим сервер на 8000 порту, режим отладки включенен.
